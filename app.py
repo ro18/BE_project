0e7ed849ff1cfc913cd1d01557bc87dc8cfa8e70
Rochelle Cordeiro <rochelle.cordiro30@gmail.com>
2020-03-29 11:18:07 +0530

    first commit

---
+++ app.py
@@ -0,0 +1,73 @@
+from flask import Flask
+
+from flask_pymongo import PyMongo
+
+from bson.json_util import dumps
+
+from bson.objectid import ObjectId
+
+from flask import jsonify, request, render_template, url_for
+
+from werkzeug.security import generate_password_hash, check_password_hash
+
+app = Flask(__name__)
+
+app.secret_key = "secretkey"
+
+app.config['MONGO_URI'] = "mongodb://localhost:27017/interview_training"
+
+mongo = PyMongo(app)
+
+
+@app.route('/')
+def index():
+    return render_template("home.html")
+
+@app.route('/student')
+def student():
+    return render_template("student.html")
+
+@app.route('/admin')
+def admin():
+    return render_template("login.html")
+
+@app.route('/signupForm')
+def adminSignupForm():
+    return render_template("signup.html")
+
+@app.route('/studentAccess',methods=['POST'])
+def studentLogin():
+    values = request.form
+    _name = values['name']
+    _email = values['email']
+    _profile = values['profile']
+    if 'resume' in request.files:
+        resume = request.files['resume']
+        mongo.save_file(resume.filename, resume)
+    if _name and _profile and _email and request.method == "POST":
+        id = mongo.db.student.insert(
+            {"name": _name,"email":_email, "profile": _profile, "resume": resume.filename})
+    return render_template("student2.html")
+
+@app.route('/adminAccess',methods=['POST'])
+def adminLogin():
+    values = request.form
+    _username = values['username']
+    _password = values['pass']
+    if _username  and _password and request.method == "POST":
+        valid = mongo.db.admin.find_one({"username": _username, "pass": _password})
+        if(valid):
+            return render_template("admin.html")
+        else:
+            return render_template('login.html')
+    
+
+@app.route('/signup', methods=['POST'])
+def adminSignup():
+    values = request.form
+    _username = values['username']
+    _email = values['email']
+    _password = values['pass']
+    if _username and _email and _password and request.method == "POST":
+        id = mongo.db.admin.insert({"username": _username, "email": _email, "pass": _password})
+    return render_template("login.html")
